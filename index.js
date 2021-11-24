99cb008877552a544ef18597e243fb44432eaf86
MichaelMcGilvray <mikeymcgilvray@gmail.com>
2021-11-23 23:00:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const url = window.location.href;
 var clipURL = url.split("clipURL=").pop();
-console.log(clipURL);
+// console.log(clipURL);
 
 
 const iframe = document.getElementsByTagName("iframe");
 iframe.src = clipURL + "&parent=michaelmcgilvray.github.io";
+
+console.log(iframe.src);
