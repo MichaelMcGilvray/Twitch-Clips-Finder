38bd2f4c5d6c83650c9560891fe93013deb0684b
MichaelMcGilvray <mikeymcgilvray@gmail.com>
2021-11-24 13:59:26 -0500

    bug fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var clipURL = url.split("clipURL=").pop();
 // console.log(clipURL);
 
 
-var iframe = document.getElementsByTagName("iframe");
+var iframe = document.getElementsByTagName("iframe")[0];
 iframe.src = clipURL + "&parent=michaelmcgilvray.github.io";
 
 console.log(iframe.src);
