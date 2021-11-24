732cc64a2f4431806a3089258f99e1f3ea4d8f26
MichaelMcGilvray <mikeymcgilvray@gmail.com>
2021-11-23 23:05:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var clipURL = url.split("clipURL=").pop();
 // console.log(clipURL);
 
 
-const iframe = document.getElementsByTagName("iframe");
+var iframe = document.getElementsByTagName("iframe");
 iframe.src = clipURL + "&parent=michaelmcgilvray.github.io";
 
 console.log(iframe.src);
