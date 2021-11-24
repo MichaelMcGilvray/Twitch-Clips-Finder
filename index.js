fce7223632a5069b3709f783a72c28df1a1a6477
MichaelMcGilvray <mikeymcgilvray@gmail.com>
2021-11-24 14:55:08 -0500

    disabled fullscreen

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const url = window.location.href;
 var clipURL = url.split("clipURL=").pop();
-// console.log(clipURL);
 
 
 var iframe = document.getElementsByTagName("iframe")[0];
 iframe.src = clipURL + "&parent=michaelmcgilvray.github.io";
-
-console.log(iframe.src);
